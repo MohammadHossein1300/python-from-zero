705c797fdcd8d112105a3da972c5bebce99c4a0c
Mohammad Hossein <mohammadhossein.hf.2000@gmail.com>
2026-06-30 19:34:36 +0330

    Add first input challenge

---
+++ 02_input/01_challenge.py
@@ -0,0 +1,11 @@
+first_name=input("Enter your first name:")
+last_name=input("Enter your last name:")
+age=int(input("Enter your age:"))
+city=input("Enter your city:")
+
+print("STUDENT INFORMATION")
+
+print("first name:",first_name)
+print("lastname:",last_name)
+print("age:",age)
+print("city:",city)

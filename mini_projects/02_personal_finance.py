cdecc157be4699d838367c981bdbaa837c08b655
Mohammad Hossein <mohammadhossein.hf.2000@gmail.com>
2026-07-01 20:49:23 +0330

    Add personal finance calculator mini project

---
+++ mini_projects/02_personal_finance.py
@@ -0,0 +1,23 @@
+first_name = (input("Enter Your Name:"))
+
+monthly_income = int (input("Enter Your Monthly Income:"))
+
+monthly_fee = int (input("Enter YOur Monthly Fee:"))
+
+saving = (monthly_income - monthly_fee)
+
+print("=====================================")
+
+print("PERSONAL FINANCE")
+
+print("=====================================")
+
+print("Name:",first_name)
+
+print("Income:",monthly_income)
+
+print("Expence:",monthly_fee)
+
+print ("Saving:",saving)
+
+print("=====================================")

dd4f14d67cf35fd20286e259a3eebb96272c8306
Mohammad Hossein <mohammadhossein.hf.2000@gmail.com>
2026-07-11 00:01:37 +0330

    Add rock paper scissors mini project

---
+++ mini_projects/11_rock_paper_scissors.py
@@ -0,0 +1,10 @@
+while True:
+
+    player_choice = (input("chosse one (Rock/Paper/Scissors):"))
+
+    if player_choice == "paper":
+     print("You win!")
+    elif player_choice == "rock":
+     print("Draw!")
+    else:
+     print("You lose!")
